6811b825bfe2492c4fc28f5d3fecb2a9e9e55c48
ClifftonK <clif.kariuki@gmail.com>
2017-08-03 18:11:16 +0300

    Finished creating page class for web elements for the Menu Toggle Test Case

---
+++ src/test/java/pages/MenuBarToggle.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by clifftonkariuki on 8/3/17.
+ */
+public class MenuBarToggle {
+
+    WebDriver driver;
+
+    public MenuBarToggle(WebDriver ldriver){this.driver= ldriver;}
+
+    public void clickMenuItems(){
+
+        driver.findElement(By.xpath("//a[contains(@href, 'nid.php')]")).click(); //this is ID Card xpath on home.php
+        driver.findElement(By.xpath("//a[contains(@href, 'nhif.php')]")).click(); //this is NHIF Card xpath on nid.php
+        driver.findElement(By.xpath("//a[contains(@href, 'nssf.php')]")).click(); //this is NSSF Card xpath on nhif.php
+        driver.findElement(By.xpath("//a[contains(@href, 'dl.php')]")).click(); //this is Driving License xpath on nssf.php
+        driver.findElement(By.linkText("Go Back Home")).click(); //this is link text to go back home from dl.php
+
+    }
+}
